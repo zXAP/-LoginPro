fe3e497a49004443cab95f425dc7c242b567784d
zXAP <107319902+zXAP@users.noreply.github.com>
2021-03-19 19:49:17 +0000

    fix test name

--- tessnet2/Program.cs
+++ tessnet2/Program.cs
@@ -12,4 +12,4 @@ namespace TesseractConsole
         {
             // Code usage sample
             Ocr ocr = new Ocr();
-            using (Bitmap bmp = new B
+            using (Bitmap bmp = new Bitma
