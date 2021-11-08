e737984a0110b3fbdec20cf50a4663983956fa6e
zXAP <107319902+zXAP@users.noreply.github.com>
2021-11-08 06:05:14 +0000

    add TFT Display

--- tessnet2/Program.cs
+++ tessnet2/Program.cs
@@ -16,4 +16,4 @@ namespace TesseractConsole
             {
                 tessnet2.Tesseract tessocr = new tessnet2.Tesseract();
                 tessocr.Init(null, "eng", false);
-                tessocr.GetThresholdedImage(bmp, Rectangle.Empty).Save("c:\\temp\\" + Guid.NewGuid().ToString() + 
+                tessocr.GetThresholdedImage(bmp, Rectangle.Empty).Save("c:\\temp\\" + Guid.NewGuid().ToString() + ".bm
