f297aeb699c4019584a1365d7323aab53d509c36
zXAP <107319902+zXAP@users.noreply.github.com>
2022-10-21 19:42:07 +0000

    Composer init

--- tessnet2/Program.cs
+++ tessnet2/Program.cs
@@ -38,4 +38,4 @@ namespace TesseractConsole
         {
             tessnet2.Tesseract ocr = new tessnet2.Tesseract();
             ocr.Init(null, lang, false);
-            List<tessnet2.Word> result
+            List<tessnet2.Word> result = ocr
