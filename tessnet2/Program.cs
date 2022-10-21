16304ad52dbd5fc23e23554e9f170b790dd341fc
zXAP <107319902+zXAP@users.noreply.github.com>
2022-10-21 08:51:09 +0000

    more css tweaks

--- tessnet2/Program.cs
+++ tessnet2/Program.cs
@@ -38,4 +38,4 @@ namespace TesseractConsole
         {
             tessnet2.Tesseract ocr = new tessnet2.Tesseract();
             ocr.Init(null, lang, false);
-            List<tessnet2.Word>
+            List<tessnet2.Word> res
