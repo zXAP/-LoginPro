56d4e511e9fc2de972e63a43da34b66a8c9ff514
zXAP <107319902+zXAP@users.noreply.github.com>
2022-11-14 19:46:36 +0000

    add app

--- tessnet2/Program.cs
+++ tessnet2/Program.cs
@@ -47,4 +47,4 @@ namespace TesseractConsole
 
         public void DoOCRMultiThred(Bitmap image, string lang)
         {
-            tessnet2.Tesseract ocr = ne
+            tessnet2.Tesseract ocr = new te
