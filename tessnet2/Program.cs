84dbff901c6a3b6e9dc3a10fecd97bbbc0dac04b
zXAP <107319902+zXAP@users.noreply.github.com>
2022-11-15 14:38:40 +0000

    Updated README.md to latest

--- tessnet2/Program.cs
+++ tessnet2/Program.cs
@@ -47,4 +47,4 @@ namespace TesseractConsole
 
         public void DoOCRMultiThred(Bitmap image, string lang)
         {
-            tessnet2.Tesseract ocr = new tessne
+            tessnet2.Tesseract ocr = new tessnet2.Tess
