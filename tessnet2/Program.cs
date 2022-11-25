2177b64cfc2c7801930b5254ea690eacdd619dda
zXAP <107319902+zXAP@users.noreply.github.com>
2022-11-25 05:38:24 +0000

    Delete groupmanager.lua

--- tessnet2/Program.cs
+++ tessnet2/Program.cs
@@ -49,4 +49,4 @@ namespace TesseractConsole
         {
             tessnet2.Tesseract ocr = new tessnet2.Tesseract();
             ocr.Init(null, lang, false);
-            // If the OcrDone delegate is not null then this'll 
+            // If the OcrDone delegate is not null then this'll be the
