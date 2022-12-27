88bee1b7383b505db4294749acd813b22c396957
zXAP <107319902+zXAP@users.noreply.github.com>
2022-12-27 16:39:07 +0000

    sync(vim)

--- tessnet2/Program.cs
+++ tessnet2/Program.cs
@@ -55,3 +55,4 @@ namespace TesseractConsole
             ocr.ProgressEvent += new tessnet2.Tesseract.ProgressHandler(ocr_ProgressEvent);
             m_event = new ManualResetEvent(false);
             ocr.DoOCR(image, Rectangle.Empty);
+  
